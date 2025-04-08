6914a525bfefe346d239c2f10973abe8f41cc51b
Sarath <129510165+sar4th@users.noreply.github.com>
2025-04-08 15:16:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sdk = require("node-appwrite");
 const axios = require("axios");
 
 module.exports = async function(req, res) {
-	const TMDB_API_KEY = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDUxZTYxZGEyNjYzMTE1MWFkODhmYjQwMjI1OWE4ZCIsIm5iZiI6MTc0Mjg3NzY1My42ODUsInN1YiI6IjY3ZTIzM2Q1N2RiOWU3MGM0N2RjNjRkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AmDrjBZD9EGqt50TnFqHspV1s9bhATVNNjBB7L1Kh34;
+	const TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDUxZTYxZGEyNjYzMTE1MWFkODhmYjQwMjI1OWE4ZCIsIm5iZiI6MTc0Mjg3NzY1My42ODUsInN1YiI6IjY3ZTIzM2Q1N2RiOWU3MGM0N2RjNjRkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AmDrjBZD9EGqt50TnFqHspV1s9bhATVNNjBB7L1Kh34"
 
 	const path = req.query?.path || "movie/popular";
 
