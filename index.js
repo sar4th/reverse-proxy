957127151354248166875c0825429b70b17baf4b
Sarath <129510165+sar4th@users.noreply.github.com>
2025-04-08 16:14:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const fetch = require('node-fetch');
-
 module.exports = async function ({ req, res, log, err }) {
   const path = req.query.path || 'movie/popular';
   const page = req.query.page || '1';
-
   const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN; // JWT Token
   const url = `https://api.themoviedb.org/3/${path}?page=${page}`;
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${TMDB_API_TOKEN}`
-  }
-};
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${TMDB_API_TOKEN}`
+    }
+  };
  
   try {
     log(`Calling TMDB: ${url}`);
     
-    const response = await fetch(url, options)
-  .then(res => res.json())
-
+    // Method 1: Using async/await
+    const response = await fetch(url, options);
     const data = await response.json();
     return res.json(data);
+    
+    // OR Method 2: Using .then() chain (choose one approach, not both)
+    // const data = await fetch(url, options)
+    //   .then(res => res.json());
+    // return res.json(data);
   } catch (error) {
     console.error('TMDB error:', error);
     return res.send('TMDB Fetch failed', 500);
