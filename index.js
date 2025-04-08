b5081c9b93b883143e73023c2f4c60118fee0034
Sarath <129510165+sar4th@users.noreply.github.com>
2025-04-08 15:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const sdk = require("node-appwrite");
-const axios = require("axios");
+import fetch from 'node-fetch';
 
-module.exports = async function(req, res) {
-	const TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDUxZTYxZGEyNjYzMTE1MWFkODhmYjQwMjI1OWE4ZCIsIm5iZiI6MTc0Mjg3NzY1My42ODUsInN1YiI6IjY3ZTIzM2Q1N2RiOWU3MGM0N2RjNjRkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AmDrjBZD9EGqt50TnFqHspV1s9bhATVNNjBB7L1Kh34"
+export default async ({ req, res }) => {
+  const path = req.query.path || 'movie/popular';
+  const page = req.query.page || '1';
 
-	const path = req.query?.path || "movie/popular";
+  const TMDB_API_KEY = process.env.TMDB_API_KEY;
+  const url = `https://api.themoviedb.org/3/${path}?api_key=${TMDB_API_KEY}&page=${page}`;
 
-	const query = req.query || {};
-	delete query.path;
-
-	  try {
-    // Call the TMDB endpoint using axios
-    const response = await axios.get(`https://api.themoviedb.org/3/${path}`, {
-      params: { ...query, api_key: TMDB_API_KEY },
-    });
-    // Return the data received from TMDB
-    res.json(response.data);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return res.json(data);
   } catch (err) {
-    // In case of error, send a JSON error response
-    res.json(
-      {
-        error: "Failed to fetch from TMDB",
-        details: err.message,
-      },
-      500
-    );
+    return res.send('TMDB Fetch failed', 500);
   }
 };
