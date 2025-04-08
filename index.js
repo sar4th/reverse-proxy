7e805671a3b2c149230acfe96ac8abd356fe08ac
Sarath <129510165+sar4th@users.noreply.github.com>
2025-04-08 16:11:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ module.exports = async function ({ req, res, log, err }) {
 
   const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN; // JWT Token
   const url = `https://api.themoviedb.org/3/${path}?page=${page}`;
-
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${TMDB_API_TOKEN}`
+  }
+};
+ 
   try {
     log(`Calling TMDB: ${url}`);
     
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${TMDB_API_TOKEN}`,
-        Accept: 'application/json',
-      },
-    });
+    const response = await fetch(url, options)
+  .then(res => res.json())
 
     const data = await response.json();
     return res.json(data);
