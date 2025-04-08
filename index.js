5fa19c625ac730102955713df7069b4d0b9ec34a
Sarath <129510165+sar4th@users.noreply.github.com>
2025-04-08 19:36:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 const fetch = require('node-fetch');
-module.exports = async function ({ req, res, log, err }) {
 
+module.exports = async function ({ req, res, log }) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  // Handle preflight OPTIONS request
+
   if (req.method === 'OPTIONS') {
     return res.send('', 204);
   }
-  const path = req.query.path || 'movie/popular';
-  const page = req.query.page || '1';
-  const TMDB_API_TOKEN =process.env.TMDB_API_TOKEN
-const url = `https://api.themoviedb.org/3/${path}${path.includes('?') ? '&' : '?'}page=${page}`;
+
+  const { path = 'movie/popular', ...queryParams } = req.query;
+
+  const queryString = new URLSearchParams(queryParams).toString();
+  const url = `https://api.themoviedb.org/3/${path}?${queryString}`;
+
+  const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: Bearer ${TMDB_API_TOKEN}
-    }
+      Authorization: `Bearer ${TMDB_API_TOKEN}`,
+    },
   };
- 
+
   try {
-    log(Calling TMDB: ${url});
-    
-    // Method 1: Using async/await
+    log(`Calling TMDB: ${url}`);
     const response = await fetch(url, options);
     const data = await response.json();
     return res.json(data);
-    
-    // OR Method 2: Using .then() chain (choose one approach, not both)
-    // const data = await fetch(url, options)
-    //   .then(res => res.json());
-    // return res.json(data);
   } catch (error) {
     console.error('TMDB error:', error);
     return res.send('TMDB Fetch failed', 500);
